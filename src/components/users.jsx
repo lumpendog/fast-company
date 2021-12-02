180ffed524e29d45c595be5cbcd4fa86f8f1c98a
lumpendog <roman.adamchik@gmail.com>
2021-12-02 14:56:45 +0300

    initial commit

---
+++ src/components/users.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import api from '../api';
+
+const Users = () => {
+  console.log(api.users.fetchAll());
+  return (
+    <>
+      <h1>Users</h1>
+    </>
+  );
+};
+
+export default Users;
